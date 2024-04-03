9889a5532c35cdaade65ce2a0dad28d78e5b557d
Abdisa Dev <abdisaDev@gmail.com>
2024-04-03 22:18:24 +0300

    setuped all nessacry tools to begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Alert } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
-import { AlertDemo } from "./components/test";
 function App() {
   return (
     <>
       <div>
-        <AlertDemo />
+        <h1>Hello World</h1>
       </div>
     </>
   );
