59cc773cdf0c2ff7d8dc64124486725397ced03e
AbdisaDev <AbdisaDev@gmail.com>
2024-04-05 08:50:42 +0300

    terms and conditions and already registered link added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { LoginForm } from "./components/loginForm/loginForm";
 import RegistrationForm from "./components/registrationForm/RegistrationForm";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 
@@ -6,8 +5,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="ems-ui-theme">
       <div className="flex justify-center">
-        <LoginForm />
-        {/* <RegistrationForm /> */}
+        <RegistrationForm />
       </div>
     </ThemeProvider>
   );
