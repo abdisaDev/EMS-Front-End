291a39a2dc212acdc41a8b53c661580512189269
Abdisa Dev <abdisaDev@gmail.com>
2024-04-07 00:35:14 +0300

    qr camera implemented

--- src/components/qrCodeReader/qrCodeReader.tsx
+++ src/components/qrCodeReader/qrCodeReader.tsx
@@ -10,14 +10,16 @@ import QrReader from "react-qr-scanner";
 
 export default function QrCodeReader() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen  ">
       <Card>
         <CardHeader>
-          <div>
+          <div className="relative">
             <QrReader
               delay={100}
-              className="rounded-2xl border-4 border-slate-400"
+              className="rounded-2xl border-4 border-slate-400 animate-jump animate-infinite animate-ease-in"
+              facingMode="rear"
             />
+            <div className="absolute border-4  rounded-xl border-slate-400 w-1/2 h-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
           </div>
         </CardHeader>
         <CardFooter className="flex flex-col space-y-4">
