3f61f99c1067e050f16dc28e5b68d796c7028677
Abdisa Dev <abdisaDev@gmail.com>
2024-04-07 00:16:09 +0300

    qr camera implemented

--- src/components/qrCodeReader/qrCodeReader.tsx
+++ src/components/qrCodeReader/qrCodeReader.tsx
@@ -1,28 +1,32 @@
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+// @ts-expect-error -> the below package didn't have any declarations
+import QrReader from "react-qr-scanner";
 
 export default function QrCodeReader() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Card>
         <CardHeader>
-          <CardTitle>QR Code - Scan for details</CardTitle>
-          <CardDescription>
-            Use your phone's camera to read the code.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex items-center justify-center">
-          <div className="blue-tile bg-slate-200 rounded-2xl py-24 w-full md:min-h-max md:w-4/5 flex justify-center text-center border-slate-300 border-4">
-            <p className="font-bold">QR Code Scanner</p>
+          <div>
+            <QrReader
+              delay={100}
+              className="rounded-2xl border-4 border-slate-400"
+            />
           </div>
-        </CardContent>
-        <CardFooter>
+        </CardHeader>
+        <CardFooter className="flex flex-col space-y-4">
+          <div>
+            <CardTitle>QR Code - Scan for details</CardTitle>
+            <CardDescription>
+              Use your phone's camera to read the code.
+            </CardDescription>
+          </div>
           <div className="flex justify-center w-full ">
             <p className="animate-bounce animate-infinite">Scanning . . .</p>
           </div>
