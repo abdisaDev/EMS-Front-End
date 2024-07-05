b1432e071076dded95436a6c2dd4de3e6975d43d
Abdisa Dev <AbdisaDev@gmail.com>
2024-07-05 11:44:53 +0300

    service worker

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.ts')
+      .register('/sw.js')
       .then((registration) => {
         console.log('Service worker registered: ', registration.scope);
       })
