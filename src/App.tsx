9e14fada20d301f0a781849a3ebca35102bb5139
Abdisa Dev <AbdisaDev@gmail.com>
2024-07-05 12:26:09 +0300

    sdsadsa

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Toaster, toast } from 'sonner';
 import ErrorPage from './pages/errorPage/errorPage';
 
+const networkStatusHandler = (status: boolean) => {
+  if (status) {
+    toast.success('Back To Online');
+    toast.dismiss();
+    window.addEventListener('offline', () => networkStatusHandler(false));
+  } else {
+    toast.error("You're Offline", { duration: Infinity });
+  }
+};
 function App() {
   const router = createBrowserRouter(routes);
-  const toastHandler = (title: string, status: boolean) => {
-    return status ? toast.success(title) : toast.error(title);
-  };
-  window.addEventListener('online', () => {
-    toastHandler('Back To Online', true);
-  });
 
-  window.addEventListener('offline', () => {
-    toastHandler("You're Offline", false);
-  });
+  window.addEventListener('online', () => networkStatusHandler(true));
+  window.removeEventListener('offline', () => networkStatusHandler(false));
 
   return (
     <ThemeProvider defaultTheme='light' storageKey='ems-ui-theme'>
