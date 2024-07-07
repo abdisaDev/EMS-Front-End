e1766289d4f56a6a56bbc3e6d701d91d23a85c18
Abdisa Dev <abdisaDev@gmail.com>
2024-07-07 07:35:23 +0300

    dsa

--- src/pages/loginFormPage/loginForm.tsx
+++ src/pages/loginFormPage/loginForm.tsx
@@ -1,12 +1,12 @@
 // validation
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 // Form Handler
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 // Shadcn Components
-import { Input } from '@/components/ui/input';
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
@@ -14,24 +14,24 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
-import { Separator } from '@/components/ui/separator';
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '@/components/ui/tooltip';
+} from "@/components/ui/tooltip";
 // react router
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 // reacptcha v3
 import {
   GoogleReCaptchaProvider,
   // useGoogleReCaptcha,
-} from 'react-google-recaptcha-v3';
-import axios from 'axios';
-import { Button } from '@/components/ui/button';
+} from "react-google-recaptcha-v3";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 export default function LoginForm() {
   // const { executeRecaptcha } = useGoogleReCaptcha();
@@ -50,8 +50,8 @@ export default function LoginForm() {
   const formSchema = z.object({
     phone_number: z
       .string()
-      .min(9, 'Invalid Phone Number.')
-      .max(10, 'Invalid Phone Number.'),
+      .min(9, "Invalid Phone Number.")
+      .max(10, "Invalid Phone Number."),
     password: z.string(),
   });
 
@@ -62,8 +62,8 @@ export default function LoginForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      phone_number: '',
-      password: '',
+      phone_number: "",
+      password: "",
     },
   });
 
@@ -73,59 +73,59 @@ export default function LoginForm() {
         phone_number,
         password,
       })
-      .then((response) => {
-        console.log(response);
-        localStorage.setItem('access_token', response.data.access_token);
-        localStorage.setItem('role', response.data.role);
-        localStorage.setItem('userId', response.data.userId);
-        localStorage.setItem('name', response.data.name);
-
-        localStorage.getItem('role') === 'admin'
-          ? navigate('/register')
-          : navigate('/home');
+      .then(async (response) => {
+        const { access_token, role, userId, name } = await response.data;
+        localStorage.clear();
+        localStorage.setItem("access_token", access_token);
+        localStorage.setItem("role", role);
+        localStorage.setItem("userId", userId);
+        localStorage.setItem("name", name);
       });
+    localStorage.getItem("role") === "admin"
+      ? navigate("/register")
+      : navigate("/home");
   };
 
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={`${import.meta.env.VITE_SITE_KEY_EMS}`}
     >
-      <div className='w-full h-screen flex justify-center items-center absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]'>
+      <div className="w-full h-screen flex justify-center items-center absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className='relative space-y-5 w-10/12 sm:w-8/12 md:w-6/12 lg:w-4/12 py-3 px-4 rounded-lg bg-[#f7f7f7]'
+            className="relative space-y-5 w-10/12 sm:w-8/12 md:w-6/12 lg:w-4/12 py-3 px-4 rounded-lg bg-[#f7f7f7]"
           >
-            <p className='text-center font-black text-2xl text-[#0f172a]'>
+            <p className="text-center font-black text-2xl text-[#0f172a]">
               Sign In
             </p>
-            <div className='space-y-3'>
-              <div className='w-full'>
+            <div className="space-y-3">
+              <div className="w-full">
                 <FormField
                   control={form.control}
-                  name='phone_number'
+                  name="phone_number"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Phone Number</FormLabel>
                       <FormControl>
-                        <Input placeholder='+25198824 * * *' {...field} />
+                        <Input placeholder="+25198824 * * *" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
               </div>
-              <div className='w-full'>
+              <div className="w-full">
                 <FormField
                   control={form.control}
-                  name='password'
+                  name="password"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Password</FormLabel>
                       <FormControl>
                         <Input
-                          placeholder='* * * * * * * *'
-                          type='password'
+                          placeholder="* * * * * * * *"
+                          type="password"
                           {...field}
                         />
                       </FormControl>
@@ -135,14 +135,14 @@ export default function LoginForm() {
                 />
               </div>
             </div>
-            <div className='w-full flex justify-end items-center'>
+            <div className="w-full flex justify-end items-center">
               <div>
                 <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger>
-                      <div className='flex justify-end'>
-                        <div className='text-sky-500 underline text-sm flex items-center mr-[20px]'>
-                          <Link to='#'>Forgot password ?</Link>
+                      <div className="flex justify-end">
+                        <div className="text-sky-500 underline text-sm flex items-center mr-[20px]">
+                          <Link to="#">Forgot password ?</Link>
                         </div>
                       </div>
                     </TooltipTrigger>
@@ -155,24 +155,24 @@ export default function LoginForm() {
             </div>
             <div>
               <Button
-                type='submit'
-                className='w-full'
+                type="submit"
+                className="w-full"
                 // onClick={handleReCaptchaVerify}
               >
                 Login
               </Button>
 
               <div>
-                <Separator className='my-4' />
+                <Separator className="my-4" />
               </div>
-              <div className='flex justify-center text-sm'>
-                <p className='w-10/12 text-center font-light	'>
+              <div className="flex justify-center text-sm">
+                <p className="w-10/12 text-center font-light	">
                   By clicking continue, you agree to our &nbsp;
-                  <Link to='#' className='text-sky-500 underline'>
+                  <Link to="#" className="text-sky-500 underline">
                     Terms of Service
                   </Link>
                   &nbsp; and &nbsp;
-                  <Link to='#' className='text-sky-500 underline'>
+                  <Link to="#" className="text-sky-500 underline">
                     Privacy Policy.
                   </Link>
                 </p>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,50 +1,50 @@
-import RegistrationForm from '@/pages/registrationFormPage/RegistrationForm';
-import LoginForm from '@/pages/loginFormPage/loginForm';
-import LandingPage from '@/pages/landingPage';
-import QrCodeReader from '@/components/qrCodeReader/qrCodeReader';
-import SecurityGuardHomePage from '@/pages/securityGuardHomePage/securityGuardHomePage';
-import UserHomePage from '@/pages/userHomePage/userHomePage';
-import ErrorPage from '@/pages/errorPage/errorPage';
+import RegistrationForm from "@/pages/registrationFormPage/RegistrationForm";
+import LoginForm from "@/pages/loginFormPage/loginForm";
+import LandingPage from "@/pages/landingPage";
+import QrCodeReader from "@/components/qrCodeReader/qrCodeReader";
+import SecurityGuardHomePage from "@/pages/securityGuardHomePage/securityGuardHomePage";
+import UserHomePage from "@/pages/userHomePage/userHomePage";
+import ErrorPage from "@/pages/errorPage/errorPage";
 
 export const routes = [
-  { path: '/', element: <LandingPage /> },
+  { path: "/", element: <LandingPage /> },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginForm />,
   },
   {
-    path: '/register',
+    path: "/register",
     element:
-      localStorage.getItem('role') === 'security_guard' ||
-      localStorage.getItem('role') === 'admin' ? (
+      localStorage.getItem("role") === "security_guard" ||
+      localStorage.getItem("role") === "admin" ? (
         <RegistrationForm />
       ) : (
         <ErrorPage />
       ),
   },
   {
-    path: '/verify-user',
+    path: "/verify-user",
     element:
-      localStorage.getItem('access_token') &&
-      localStorage.getItem('role') === 'security_guard' ? (
+      localStorage.getItem("access_token") &&
+      localStorage.getItem("role") === "security_guard" ? (
         <QrCodeReader />
       ) : (
         <ErrorPage />
       ),
   },
   {
-    path: '/home',
+    path: "/home",
     element:
-      localStorage.getItem('role') === 'security_guard' ? (
+      localStorage.getItem("role") === "security_guard" ? (
         <SecurityGuardHomePage />
-      ) : localStorage.getItem('role') === 'user' ? (
+      ) : localStorage.getItem("role") === "user" ? (
         <UserHomePage />
-      ) : localStorage.getItem('role') === 'admin' ||
-        !localStorage.getItem('access_token') ? (
+      ) : localStorage.getItem("role") === "admin" ||
+        !localStorage.getItem("access_token") ? (
         <ErrorPage />
       ) : (
         <></>
       ),
   },
-  { path: '*', element: <ErrorPage /> },
+  { path: "*", element: <ErrorPage /> },
 ];
